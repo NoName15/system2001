67fad08223093cc6e0ac96ba62f706479aa993f7
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-11-10 11:37:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,24 @@ const client = new Discord.Client();
 const prefix = ">";
 const moment = require('moment');
 
+var cats = [
+
+"https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+"https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
+"http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg",
+"https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg",
+"https://vignette1.wikia.nocookie.net/houseofnight/images/8/8b/Cats.jpg/revision/latest?cb=20130812053537",
+"https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg",
+"http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg"
+]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'cat')) {
+         var cat = new Discord.RichEmbed()
+.setImage(`cats[Math.floor(Math.random() * cats.length)]`)
+message.channel.sendEmbed(cat);
+    }
+});
 client.on('message',   ro7 =>{ // Edited by [ @RO7#9078 ]
 
     var  args = ro7.content.split(" ").slice(2).join(" ")
