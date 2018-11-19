21c8bdab64fa219a9e4bc2dd8f1e8ce0e0a5e3f2
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-11-19 20:44:35 +0100

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: node bot.js
+worker2: node bot2.js
---
+++ bot2.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const moment = require("moment")
+
+
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 21) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+    
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+}); 
+
+
+
+client.login(process.env.BOT_TOKEN2);// لا تغير فيها شيء
